c98a492cbb0f7c9aa65d5fbd2e8e32ade49e54cf
Christopher Britt <christopher.brittjr@outlook.com>
2018-09-25 15:23:22 -0400

    Add project files.

---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Controllers;
+using System.Text;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { set; get; }
+    }
+}
---
+++ LanguageFeatures/Models/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Controllers;
+using System.Text;
+using LanguageFeatures.Models;
+using System.Collections;
+
+namespace LanguageFeatures.Models
+{
+    public class ShoppingCart: IEnumerable<Product>
+    {
+        public List<Product> Products { get; set; }
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
